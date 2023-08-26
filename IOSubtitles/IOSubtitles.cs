51940ccec35196735a1fed092e629996b5eb2e6b
Yannick Strauß <yannickstrauss@gmail.com>
2023-08-26 22:41:56 +0200

    Projektdateien hinzufügen.

---
+++ IOSubtitles/IOSubtitles.cs
@@ -0,0 +1,136 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IOSubtitles
+{
+    [BepInEx.BepInPlugin(_guid, "IOSubtitles", "1.0.0")]
+    public class Hook : BaseUnityPlugin
+    {
+        private const string _guid = "rieght.insultorder.iosubtitles";
+
+        public Hook()
+        {
+            SceneManager.sceneLoaded += SubtitlesHook.SceneLoaded;
+            SubtitlesHook.Init();
+            var harmony = new Harmony(_guid);
+            harmony.PatchAll(typeof(SubtitlesHook));
+        }
+    }
+
+    public static class SubtitlesHook
+    {
+        private static ManualLogSource _logger = BepInEx.Logging.Logger.CreateLogSource("iosubtitles");
+
+        private static UILabel _subtitlesLabel;
+        private static GameObject _subtitleTextbox;
+
+        private static FH_AnimeController _AnimeController;
+        private static AudioSource _audiosourceNormal;
+        private static AudioSource _audiosourceUX;
+        private static AudioSource _audiosourceEjac;
+        private static AudioSource _audiosourceEjac2;
+
+        private static Dictionary<string, List<List<string>>> _subtitles = new Dictionary<string, List<List<string>>>();
+        private static float _subtitleTime = 0f;
+        private static string _subitleId;
+        private static int _subtitleLineNr = 0;
+
+        public static void Init()
+        {
+            string json = File.ReadAllText("subtitles.json", System.Text.Encoding.UTF8);
+            _subtitles = JSONDeserializer.Deserialize(json);
+        }
+
+        [HarmonyPatch(typeof(FH_AnimeController), "Update")]
+        [HarmonyPostfix]
+        public static void PlayClip()
+        {   // Init Audiosources
+            if (_audiosourceNormal == null) _audiosourceNormal = GameObject.Find("HS_Head").GetComponents<AudioSource>()[12];
+            if (_audiosourceUX == null) _audiosourceUX = GameObject.Find("HS_Head").GetComponents<AudioSource>()[13];
+            if (_audiosourceEjac == null) _audiosourceEjac = GameObject.Find("HS_Head").GetComponents<AudioSource>()[14];
+            if (_audiosourceEjac2 == null) _audiosourceEjac2 = GameObject.Find("HS_Head").GetComponents<AudioSource>()[15];
+
+            if (GameClass.UraVoice && _AnimeController.ClipName[12] != null && _subtitles.ContainsKey(_AnimeController.ClipName[12]))
+                PlaySubtitle(_AnimeController.ClipName[12], _audiosourceNormal.time);
+            else if (_AnimeController.ClipName[13] != null && _subtitles.ContainsKey(_AnimeController.ClipName[13]))
+                PlaySubtitle(_AnimeController.ClipName[13], _audiosourceUX.time);
+            else if (_AnimeController.ClipName[14] != null && _subtitles.ContainsKey(_AnimeController.ClipName[14]))
+                PlaySubtitle(_AnimeController.ClipName[14], _audiosourceEjac.time);
+            else if (_AnimeController.ClipName[15] != null && _subtitles.ContainsKey(_AnimeController.ClipName[15]))
+                PlaySubtitle(_AnimeController.ClipName[15], _audiosourceEjac2.time);
+            else
+                _subtitlesLabel.text = string.Empty;
+
+            _subtitleTextbox.transform.position = new Vector3(0, -0.75f, 0);
+        }
+
+        private static void PlaySubtitle(string clipname, float audiotime)
+        {
+            // Swap subtitles on new audioclip
+            if (_subitleId != clipname)
+            {
+                _subitleId = clipname;
+                _subtitleLineNr = 0;
+                _subtitleTime = float.Parse(_subtitles[_subitleId][_subtitleLineNr][0]);
+                _subtitlesLabel.text = _subtitles[_subitleId][_subtitleLineNr][1];
+                return;
+            }
+
+            // Swap subtitle-line when current subtitle-line is finished
+            if (audiotime >= _subtitleTime)
+            {
+                _subtitleLineNr++;
+                // Return if there are no subtitle-lines left and deactivate the subtitle-line-swapping-process
+                if (_subtitles[_subitleId].Count <= _subtitleLineNr)
+                {
+                    _subtitleTime = float.MaxValue;
+                    return;
+                }
+                _subtitleTime = float.Parse(_subtitles[_subitleId][_subtitleLineNr][0]);
+                _subtitlesLabel.text = _subtitles[_subitleId][_subtitleLineNr][1];
+                _subtitlesLabel.MakePixelPerfect();
+            }
+        }
+
+        // Add Subtitle TextBox to Scene
+        public static void SceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+        {
+            if (scene.name == "FH")
+            {
+                _subtitleTextbox = new GameObject("Subtitles_Text");
+                _subtitlesLabel = _subtitleTextbox.AddComponent<UILabel>();
+
+                UILabel fpsLabel = GameObject.Find("Label_FPS").GetComponent<UILabel>();
+                _AnimeController = GameObject.Find("MainSystem").GetComponent<FH_AnimeController>();
+                _subtitlesLabel.ambigiousFont = fpsLabel.ambigiousFont;
+                _subtitlesLabel.applyGradient = fpsLabel.applyGradient;
+                _subtitlesLabel.bitmapFont = fpsLabel.bitmapFont;
+                _subtitlesLabel.alignment = fpsLabel.alignment;
+                _subtitlesLabel.alpha = fpsLabel.alpha;
+                _subtitlesLabel.border = fpsLabel.border;
+                _subtitlesLabel.color = fpsLabel.color;
+                _subtitlesLabel.depth = fpsLabel.depth;
+                _subtitlesLabel.drawRegion = fpsLabel.drawRegion;
+                _subtitlesLabel.effectColor = fpsLabel.effectColor;
+                _subtitlesLabel.effectDistance = fpsLabel.effectDistance;
+                _subtitlesLabel.effectStyle = fpsLabel.effectStyle;
+                _subtitlesLabel.floatSpacingX = fpsLabel.floatSpacingX;
+                _subtitlesLabel.floatSpacingY = fpsLabel.floatSpacingY;
+                _subtitlesLabel.useFloatSpacing = fpsLabel.useFloatSpacing;
+                _subtitlesLabel.fontSize = fpsLabel.fontSize;
+                _subtitlesLabel.fontStyle = fpsLabel.fontStyle;
+                _subtitlesLabel.height = fpsLabel.height;
+                _subtitlesLabel.material = fpsLabel.material;
+                _subtitlesLabel.width = fpsLabel.width;
+                _subtitlesLabel.enabled = true;
+                _subtitlesLabel.multiLine = true;
+            }
+        }
+    }
+}
---
+++ IOSubtitles/JSONDeserializer.cs
@@ -0,0 +1,110 @@
+﻿using BepInEx.Logging;
+using System.Collections.Generic;
+using System.Text;
+
+// {
+//  "efe":
+//      [
+//          ["3423", "dwdw"],
+//          ["2131", "dwdwads"]
+//      ],
+//  "feffee":
+//      [
+//      ]
+//  }
+
+namespace IOSubtitles
+{
+    public class JSONDeserializer
+    {
+        private static ManualLogSource _logger = BepInEx.Logging.Logger.CreateLogSource("iosubtitles");
+
+
+        private enum DeserializeState
+        {
+            Init, KeyStart, KeyEnd, BodyStart, BodyEnd, BodyEntryStart, BodyEntryEnd, BodyEntryValueStart, BodyEntryValueEnd
+        }
+
+        public static Dictionary<string, List<List<string>>> Deserialize(string json)
+        {
+            Dictionary<string, List<List<string>>> val = new Dictionary<string, List<List<string>>>();
+            List<List<string>> body = new List<List<string>>();
+            List<string> entry = new List<string>();
+
+            string key = "";
+
+            DeserializeState deserializeState = DeserializeState.Init;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char c in json)
+            {
+                if (c == '"')
+                {
+                    switch (deserializeState)
+                    {
+                        case DeserializeState.Init:
+                            deserializeState = DeserializeState.KeyStart;
+                            break;
+                        case DeserializeState.KeyStart:
+                            deserializeState = DeserializeState.KeyEnd;
+                            key = stringBuilder.ToString();
+                            stringBuilder = new StringBuilder();
+                            break;
+                        case DeserializeState.BodyEntryStart:
+                            deserializeState = DeserializeState.BodyEntryValueStart;
+                            break;
+                        case DeserializeState.BodyEntryValueStart:
+                            deserializeState = DeserializeState.BodyEntryValueEnd;
+                            entry.Add(stringBuilder.ToString());
+                            stringBuilder = new StringBuilder();
+                            break;
+                        case DeserializeState.BodyEntryValueEnd:
+                            deserializeState = DeserializeState.BodyEntryValueStart;
+                            break;
+                        case DeserializeState.BodyEnd:
+                            deserializeState = DeserializeState.KeyStart;
+                            break;
+                    }
+                }
+                else if (c == '[')
+                {
+                    switch (deserializeState)
+                    {
+                        case DeserializeState.KeyEnd:
+                            deserializeState = DeserializeState.BodyStart;
+                            break;
+                        case DeserializeState.BodyStart:
+                            deserializeState = DeserializeState.BodyEntryStart;
+                            break;
+                        case DeserializeState.BodyEntryEnd:
+                            deserializeState = DeserializeState.BodyEntryStart;
+                            break;
+                    }
+                }
+                else if (c == ']')
+                {
+                    switch (deserializeState)
+                    {
+                        case DeserializeState.BodyEntryValueEnd:
+                            deserializeState = DeserializeState.BodyEntryEnd;
+                            body.Add(entry);
+                            entry = new List<string>();
+                            break;
+                        case DeserializeState.BodyEntryEnd:
+                            deserializeState = DeserializeState.BodyEnd;
+                            val.Add(key,body);
+                            body = new List<List<string>>();
+                            break;
+                    }
+                }
+                else if (deserializeState == DeserializeState.KeyStart || deserializeState == DeserializeState.BodyEntryValueStart)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return val;
+        }
+
+    }
+}
